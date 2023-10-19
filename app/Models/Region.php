95b6bbafc978934c7f48c1cab6a6685a8ebd46fa
Yugo <dedy.yugo.purwanto@gmail.com>
2023-10-19 22:31:18 +0700

    Add missing relation from region to district

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Region extends Model
 {
@@ -13,5 +14,10 @@ class Region extends Model
     public function country(): BelongsTo
     {
         return $this->belongsTo(Country::class);
+    }
+
+    public function districts(): HasMany
+    {
+        return $this->hasMany(District::class);
     }
 }
