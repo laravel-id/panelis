b14fe129327c466711a438341f630e3f080f819a
Yugo <dedy.yugo.purwanto@gmail.com>
2023-10-20 20:21:17 +0700

    Add inactive counter for each location

--- app/Filament/Resources/Location/Widgets/LocationStatsOverview.php
+++ app/Filament/Resources/Location/Widgets/LocationStatsOverview.php
@@ -9,12 +9,25 @@ use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class LocationStatsOverview extends BaseWidget
 {
+    protected static ?string $pollingInterval = null;
+
     protected function getStats(): array
     {
         return [
-            BaseWidget\Stat::make(__('Country'), Country::count()),
-            BaseWidget\Stat::make(__('Region'), Region::count()),
-            BaseWidget\Stat::make(__('District'), District::count()),
+            BaseWidget\Stat::make(__('Country'), Country::count())
+                ->description(__(':count inactive', [
+                    'count' => Country::whereIsActive(false)->count(),
+                ])),
+
+            BaseWidget\Stat::make(__('Region'), Region::count())
+                ->description(__(':count Inactive', [
+                    'count' => Region::whereIsActive(false)->count(),
+                ])),
+
+            BaseWidget\Stat::make(__('District'), District::count())
+                ->description(__(':count Inactive', [
+                    'count' => District::whereIsActive(false)->count()
+                ])),
         ];
     }
 }
