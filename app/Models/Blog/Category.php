efe30ea46330b75ef0789d7e2ec6241556f40553
Yugo <dedy.yugo.purwanto@gmail.com>
2023-11-11 13:10:01 +0700

    Add relation to post

--- app/Models/Blog/Category.php
+++ app/Models/Blog/Category.php
@@ -4,6 +4,7 @@ namespace App\Models\Blog;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
@@ -12,4 +13,9 @@ class Category extends Model
     use SoftDeletes;
 
     protected $table = 'blog_categories';
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'blog_category_post');
+    }
 }
