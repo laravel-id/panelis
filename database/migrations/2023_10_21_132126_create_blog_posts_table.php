74383fdf06ac469057bd79087efd0c0e88c42da2
Yugo <dedy.yugo.purwanto@gmail.com>
2023-11-16 23:20:51 +0700

    Cascade on delete for category/post relations

--- database/migrations/2023_10_21_132126_create_blog_posts_table.php
+++ database/migrations/2023_10_21_132126_create_blog_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -27,9 +26,13 @@ return new class extends Migration
             $table->softDeletes();
         });
 
-        Schema::create('blog_category_post', function (Blueprint $table){
-            $table->foreignId('category_id')->constrained('blog_categories');
-            $table->foreignId('post_id')->constrained('blog_posts');
+        Schema::create('blog_category_post', function (Blueprint $table) {
+            $table->foreignId('category_id')
+                ->constrained('blog_categories')
+                ->cascadeOnDelete();
+            $table->foreignId('post_id')
+                ->constrained('blog_posts')
+                ->cascadeOnDelete();
         });
     }
 
