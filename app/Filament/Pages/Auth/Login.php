5c553173b80fffde399e092bd2d3a3690561be48
Yugo <dedy.yugo.purwanto@gmail.com>
2023-11-18 14:51:24 +0700

    Fill username and password with demo env

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,7 +2,34 @@
 
 namespace App\Filament\Pages\Auth;
 
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\HtmlString;
+
 class Login extends \Filament\Pages\Auth\Login
 {
+    protected function getEmailFormComponent(): Component
+    {
+        return TextInput::make('email')
+            ->label(__('filament-panels::pages/auth/login.form.email.label'))
+            ->email()
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->default(env('DEMO_EMAIL'))
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
 
+    protected function getPasswordFormComponent(): Component
+    {
+        return TextInput::make('password')
+            ->label(__('filament-panels::pages/auth/login.form.password.label'))
+            ->hint(filament()->hasPasswordReset() ? new HtmlString(Blade::render('<x-filament::link :href="filament()->getRequestPasswordResetUrl()"> {{ __(\'filament-panels::pages/auth/login.actions.request_password_reset.label\') }}</x-filament::link>')) : null)
+            ->password()
+            ->autocomplete('current-password')
+            ->required()
+            ->default(env('DEMO_PASSWORD'))
+            ->extraInputAttributes(['tabindex' => 2]);
+    }
 }
