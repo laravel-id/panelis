2faea1a94304e11a9dce4ce20c7d467649f51b8e
Yugo <dedy.yugo.purwanto@gmail.com>
2024-05-27 10:08:36 +0700

    Handling errror when import/export is error

--- app/Filament/Resources/TranslationResource/Pages/ListTranslations.php
+++ app/Filament/Resources/TranslationResource/Pages/ListTranslations.php
@@ -4,12 +4,17 @@ namespace App\Filament\Resources\TranslationResource\Pages;
 
 use App\Filament\Resources\TranslationResource;
 use App\Models\Translation;
+use Exception;
 use Filament\Actions;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Toggle;
+use Filament\Notifications\Actions\Action as NotificationAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ListTranslations extends ListRecords
@@ -46,21 +51,56 @@ class ListTranslations extends ListRecords
                 ])
                 ->action(function (array $data): void {
                     $lines = json_decode($data['trans']->getContent(), associative: true);
-                    foreach ($lines as $index => $line) {
-                        [$group, $key] = explode('.', $index, 2);
+                    try {
+                        foreach ($lines as $index => $line) {
+                            [$group, $key] = explode('.', $index, 2);
 
-                        $trans = Translation::firstOrNew([
-                            'group' => $group,
-                            'key' => $key,
-                        ]);
+                            $trans = Translation::firstOrNew([
+                                'group' => $group,
+                                'key' => $key,
+                            ]);
 
-                        $newLine = [$data['locale'] => $line];
-                        if (! empty($trans->text)) {
-                            $trans->text = array_merge($trans->text, $newLine);
-                        } else {
-                            $trans->text = $newLine;
+                            $newLine = [$data['locale'] => $line['text']];
+
+                            $trans->is_system = $line['is_system'];
+                            if (! empty($trans->text)) {
+                                $trans->text = array_merge($trans->text, $newLine);
+                            } else {
+                                $trans->text = $newLine;
+                            }
+                            $trans->save();
                         }
-                        $trans->save();
+
+                        Notification::make()
+                            ->success()
+                            ->title(__('translation.file_imported'))
+                            ->send();
+                    } catch (Exception $e) {
+                        Log::error($e);
+
+                        Notification::make()
+                            ->danger()
+                            ->title(__('translation.import_failed'))
+                            ->body(__('translation.import_file_invalid'))
+                            ->persistent()
+                            ->actions([
+                                NotificationAction::make('view')
+                                    ->label(__('translation.view_template'))
+                                    ->action(function (): ?StreamedResponse {
+                                        return response()->streamDownload(function () {
+                                            $format = [
+                                                'group.key' => [
+                                                    'text' => 'Sample text',
+                                                    'is_system' => false,
+                                                ],
+                                            ];
+
+                                            echo json_encode($format);
+
+                                        }, 'localization-example.json');
+                                    }),
+                            ])
+                            ->send();
                     }
                 }),
 
@@ -77,16 +117,38 @@ class ListTranslations extends ListRecords
                             'en' => __('translation.locale_en'),
                             'id' => __('translation.locale_id'),
                         ]),
+
+                    Toggle::make('is_system')
+                        ->inline(false)
+                        ->label(__('translation.system_only')),
                 ])
                 ->action(function (array $data): ?StreamedResponse {
-                    $locale = $data['locale'];
-                    if (empty($locale)) {
-                        return null;
+                    try {
+                        $locale = $data['locale'];
+                        $isSystem = $data['is_system'] ?? false;
+
+                        if (empty($locale)) {
+                            return null;
+                        }
+
+                        Notification::make()
+                            ->success()
+                            ->title(__('translation.export_success'))
+                            ->send();
+
+                        return response()->streamDownload(function () use ($locale, $isSystem): void {
+                            echo json_encode(Translation::getFormattedTranslation($locale, $isSystem));
+                        }, sprintf('%s.json', $locale));
+                    } catch (Exception $e) {
+                        Log::error($e);
+
+                        Notification::make()
+                            ->danger()
+                            ->title(__('translation.export_failed'))
+                            ->send();
                     }
 
-                    return response()->streamDownload(function () use ($locale): void {
-                        echo json_encode(Translation::getFormattedTranslation($locale));
-                    }, sprintf('%s.json', $locale));
+                    return null;
                 }),
 
             ActionGroup::make([
--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Query\Builder;
 use Spatie\TranslationLoader\LanguageLine;
 
 class Translation extends LanguageLine
@@ -13,14 +14,20 @@ class Translation extends LanguageLine
         'is_system' => 'boolean',
     ];
 
-    public static function getFormattedTranslation(string $locale): ?array
+    public static function getFormattedTranslation(string $locale, bool $isSystem = false): ?array
     {
-        return Translation::get()
+        return Translation::orderBy('group')
+            ->orderBy('key')
+            ->when($isSystem, fn (Builder $builder): Builder => $builder->where('is_system', true))
+            ->get()
             ->mapWithKeys(function ($line) use ($locale): array {
                 $key = sprintf('%s.%s', $line->group, $line->key);
                 $text = data_get($line->text, $locale);
 
-                return [$key => $text];
+                return [$key => [
+                    'text' => $text,
+                    'is_system' => $line->is_system,
+                ]];
             })
             ->toArray();
     }
