84e8ff1cb020e938aa94538f820fe4187caa4800
Yugo <dedy.yugo.purwanto@gmail.com>
2024-05-29 17:06:10 +0700

    Ability to change number format and add currency symbol

---
+++ app/Models/Enums/NumberFormat.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Enums;
+
+use Illuminate\Support\Number;
+
+enum NumberFormat: string
+{
+    case Plain = '';
+
+    case PlainCommaDecimal = '2 , ';
+
+    case DotWithoutDecimal = '0 . ,';
+
+    case CommaWithoutDecimal = '0 , .';
+
+    case DotWithDecimal = '2 . ,';
+
+    case CommaWithDecimal = '2 , .';
+
+    public static function options(): array
+    {
+        return collect(NumberFormat::cases())
+            ->mapWithKeys(function ($format) {
+                return [$format->value => Number::money(10_000.12, format: $format->value, symbol: '')];
+            })
+            ->toArray();
+    }
+}
