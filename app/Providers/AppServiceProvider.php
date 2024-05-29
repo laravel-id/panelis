08e5a404e8f5ed5270940ecc25e0946d65cd8d6e
Yugo <dedy.yugo.purwanto@gmail.com>
2024-05-29 17:05:33 +0700

    Add number macro to display currency with selected format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Setting;
 use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Number;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,45 @@ class AppServiceProvider extends ServiceProvider
         LanguageSwitch::configureUsing(function (LanguageSwitch $lang) {
             $lang->locales(config('app.locales'))
                 ->circular();
+        });
+
+        Number::macro('money', function (
+            int|float $amount,
+            ?string $format = null,
+            ?string $symbol = null,
+            ?bool $isSymbolSuffix = null,
+        ): string {
+            $format = $format ?? config('app.number_format', '');
+            $isSymbolSuffix = $isSymbolSuffix ?? config('app.symbol_suffix', false);
+
+            if (empty($format)) {
+                if ($isSymbolSuffix) {
+                    return sprintf('%.2f %s', $amount, $symbol);
+                }
+
+                return sprintf('%s%.2f', $symbol, $amount);
+            }
+
+            $format = explode(' ', $format, 3);
+            $format = array_map(function ($value) {
+                if (is_numeric($value)) {
+                    return intval($value);
+                }
+
+                return $value;
+            }, $format);
+
+            $currency = [
+                $symbol = $symbol ?? config('app.currency_symbol'),
+                $number = number_format($amount, ...$format),
+            ];
+
+            if ($isSymbolSuffix) {
+                $placeholder = '%s %s';
+                $currency = [$number, $symbol];
+            }
+
+            return vsprintf($placeholder ?? '%s%s', $currency);
         });
     }
 
