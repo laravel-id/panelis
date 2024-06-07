9af67c4302849b39a3a915ded3fb2a0088fee7bc
Yugo <dedy.yugo.purwanto@gmail.com>
2024-06-07 17:09:55 +0700

    Fix setting override and language switcher

---
+++ app/Providers/SettingServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Setting;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class SettingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        if (!$this->app->runningInConsole()) {
+            // sometimes, when in CLI, this feature not really works
+            // for example, when deploying using Docker
+
+            $hasSetting = Cache::remember('has_setting', now()->addDay(), function (): bool {
+                return Schema::hasTable('settings');
+            });
+
+            if ($hasSetting) {
+                Setting::getAll()->each(function (Setting $setting): void {
+                    Config::set($setting->key, $setting->value);
+                });
+
+                if (Auth::check()) {
+                    Setting::getByUser(Auth::id())->each(function (Setting $setting): void {
+                        // override config from db with user's
+                       Config::set($setting->key, $setting->value);
+                    });
+                }
+            }
+
+            LanguageSwitch::configureUsing(function (LanguageSwitch $lang) {
+                $lang->locales(config('app.locales', [config('app.locale')]))
+                    ->circular();
+            });
+        }
+    }
+}
