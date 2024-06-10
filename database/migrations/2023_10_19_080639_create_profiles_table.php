7e0b19f5864ec8cae083640c77f8ab86d1bb5c27
Yugo <dedy.yugo.purwanto@gmail.com>
2024-06-10 09:41:11 +0700

    Ability to edit account, profile, and avatar

--- database/migrations/2023_10_19_080639_create_profiles_table.php
+++ database/migrations/2023_10_19_080639_create_profiles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +14,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('district_id')->nullable()
-                ->constrained()
-                ->nullOnDelete();
+            $table->foreignIdFor(User::class)->constrained();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->timestamps();
