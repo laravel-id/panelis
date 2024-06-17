c03aa69cd843a7679574a0aef0f1c6d006186d4e
Yugo <dedy.yugo.purwanto@gmail.com>
2024-06-17 18:40:37 +0700

    Minor change on schedule view layout

--- app/Filament/Resources/Event/ScheduleResource/Pages/ViewSchedule.php
+++ app/Filament/Resources/Event/ScheduleResource/Pages/ViewSchedule.php
@@ -6,7 +6,6 @@ use App\Filament\Resources\Event\ScheduleResource;
 use App\Models\Event\Package;
 use App\Models\Event\Schedule;
 use Filament\Actions;
-use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\KeyValueEntry;
 use Filament\Infolists\Components\RepeatableEntry;
@@ -16,6 +15,7 @@ use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\FontWeight;
+use Filament\Support\Enums\IconPosition;
 use Illuminate\Support\Number;
 use Illuminate\Support\Str;
 
@@ -36,53 +36,68 @@ class ViewSchedule extends ViewRecord
             ->columns(3)
             ->schema([
                 Section::make(__('event.schedule'))
+                    ->columns(2)
                     ->columnSpan(2)
                     ->schema([
                         TextEntry::make('title')
                             ->hiddenLabel()
-                            ->url(fn (Schedule $schedule): string => route('schedule.view', [
+                            ->columnSpan(2)
+                            ->url(fn(Schedule $schedule): string => route('schedule.view', [
                                 $schedule->started_at->format('Y'),
                                 $schedule->slug,
                             ]))
                             ->label(__('event.schedule_title'))
                             ->openUrlInNewTab()
+                            ->icon('heroicon-s-link')
+                            ->iconPosition(IconPosition::After)
                             ->size(TextEntry\TextEntrySize::Large)
                             ->weight(FontWeight::Bold),
 
                         TextEntry::make('description')
+                            ->columnSpan(2)
                             ->hiddenLabel()
-                            ->hidden(fn (Schedule $schedule): bool => empty($schedule->description))
+                            ->hidden(fn(Schedule $schedule): bool => empty($schedule->description))
                             ->label(__('event.schedule_description'))
                             ->markdown(),
 
                         TextEntry::make('started_at')
                             ->label(__('event.schedule_started_at'))
-                            ->date(config('app.datetime_format')),
+                            ->dateTime(config('app.datetime_format')),
 
                         TextEntry::make('finished_at')
-                            ->visible(fn (Schedule $schedule): bool => ! empty($schedule->finished_at))
-                            ->date(),
-
-                        IconEntry::make('is_virtual')
-                            ->boolean()
-                            ->label(__('event.schedule_is_virtual')),
+                            ->visible(fn(Schedule $schedule): bool => !empty($schedule->finished_at))
+                            ->dateTime(config('app.datetime_format')),
 
                         TextEntry::make('full_location')
-                            ->visible(fn (Schedule $schedule): bool => ! $schedule->is_virtual)
                             ->html()
                             ->openUrlInNewTab()
-                            ->icon('heroicon-s-map-pin')
-                            ->label(__('event.schedule_location')),
+                            ->label(__('event.schedule_location'))
+                            ->icon(fn(Schedule $schedule): string => $schedule->is_virtual ? 'heroicon-s-globe-alt' : 'heroicon-s-map-pin')
+                            ->formatStateUsing(function (Schedule $schedule): string {
+                                if ($schedule->is_virtual) {
+                                    return __('event.schedule_is_virtual');
+                                }
+
+                                return $schedule->full_location;
+                            }),
 
                         RepeatableEntry::make('contacts')
+                            ->label(__('event.schedule_contact'))
                             ->columns(3)
+                            ->columnSpan(2)
                             ->schema([
-                                TextEntry::make('name'),
-                                TextEntry::make('phone'),
-                                TextEntry::make('email'),
+                                TextEntry::make('name')
+                                    ->label(__('event.schedule_contact_name')),
+
+                                TextEntry::make('phone')
+                                    ->label(__('event.schedule_contact_phone')),
+
+                                TextEntry::make('email')
+                                    ->label(__('event.schedule_contact_email')),
                             ]),
 
                         KeyValueEntry::make('metadata')
+                            ->columnSpan(2)
                             ->label(__('event.schedule_metadata')),
                     ]),
 
@@ -92,7 +107,7 @@ class ViewSchedule extends ViewRecord
                         ImageEntry::make('poster')
                             ->height(500)
                             ->hiddenLabel()
-                            ->visible(fn (Schedule $schedule): bool => ! empty($schedule->poster))
+                            ->visible(fn(Schedule $schedule): bool => !empty($schedule->poster))
                             ->alignment(Alignment::Center)
                             ->extraImgAttributes([
                                 'alt' => 'Poster',
@@ -106,14 +121,14 @@ class ViewSchedule extends ViewRecord
                         TextEntry::make('categories')
                             ->label(__('event.schedule_category')),
 
+                        TextEntry::make('url')
+                            ->label(__('event.schedule_url'))
+                            ->url(fn(Schedule $schedule): ?string => $schedule->url)
+                            ->formatStateUsing(fn(Schedule $schedule): string => Str::limit($schedule->url, 100)),
+
                         TextEntry::make('created_at')
                             ->label(__('ui.created_at'))
                             ->since(),
-
-                        TextEntry::make('url')
-                            ->label(__('event.schedule_url'))
-                            ->url(fn (Schedule $schedule): ?string => $schedule->url)
-                            ->formatStateUsing(fn (Schedule $schedule): string => Str::limit($schedule->url, 100)),
 
                         TextEntry::make('updated_at')
                             ->label(__('ui.updated_at'))
