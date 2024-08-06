d0418a4336d476e4f6e735dbe3d6dd9679456b06
Yugo <dedy.yugo.purwanto@gmail.com>
2024-08-06 21:32:39 +0700

    Add is_root attribute for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasAvatar;
 use Filament\Models\Contracts\HasTenants;
 use Filament\Panel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -117,9 +118,11 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, HasTenant
             ->exists();
     }
 
-    public function getIsRootAccess(): bool
+    public function isRoot(): Attribute
     {
-        return $this->roles->isEmpty();
+        return new Attribute(
+            get: fn(): bool => $this->roles->isEmpty(),
+        );
     }
 
     public function roles(): MorphToMany
--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
@@ -56,9 +57,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return $user->is_root;
         });
     }
 }
