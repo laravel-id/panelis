a13f60f8c767397b9f60147cf05b1f20f4c9ef07
Yugo <dedy.yugo.purwanto@gmail.com>
2024-08-06 17:04:21 +0700

    Add warning when ping URL is empty

--- app/Actions/Ping.php
+++ app/Actions/Ping.php
@@ -3,14 +3,21 @@
 namespace App\Actions;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class Ping
 {
     use AsAction;
 
-    public function handle(string $url): void
+    public function handle(?string $url): void
     {
+        if (empty($url)) {
+            Log::warning('Ping URL is not defined.');
+
+            return;
+        }
+
         Http::throw()->get($url);
     }
 }
