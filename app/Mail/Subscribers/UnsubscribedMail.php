058bb3f3d9e5d0897b9c8de2ca3d7a7e07917a1d
Yugo <dedy.yugo.purwanto@gmail.com>
2024-08-06 21:59:30 +0700

    Queue subscribe email by default

--- app/Mail/Subscribers/UnsubscribedMail.php
+++ app/Mail/Subscribers/UnsubscribedMail.php
@@ -11,7 +11,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class UnsubscribedMail extends Mailable
+class UnsubscribedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
