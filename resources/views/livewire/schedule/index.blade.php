eaef3a96bd27accf0beeffc39a76426083766c6e
Yugo <dedy.yugo.purwanto@gmail.com>
2024-08-09 20:03:02 +0700

    Wrap index table in article

--- resources/views/livewire/schedule/index.blade.php
+++ resources/views/livewire/schedule/index.blade.php
@@ -25,50 +25,52 @@
   @endif
 
   @if (!$schedules->isEmpty())
-    <div class="overflow-auto">
-      <table>
-        <thead>
-        <tr>
-          <th scope="col">@lang('event.schedule_date')</th>
-          <th scope="col">@lang('event.schedule_title')</th>
-          <th scope="col" class="large-screen">@lang('event.schedule_categories')</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($schedules as $schedule)
+    <article>
+      <div class="overflow-auto">
+        <table>
+          <thead>
           <tr>
-            <td>
-              <div class="pico-color-{{ get_color_theme() }}-600">{{ $schedule->started_at->timezone($timezone)->translatedFormat('d') }}</div>
-              {{ $schedule->started_at->timezone($timezone)->format('M') }}<sup>{{ $schedule->started_at->timezone($timezone)->format('y') }}</sup>
-            </td>
-            <td>
-              <a href="{{ route('schedule.view', ['slug' => $schedule->slug]) }}" class="{{ $schedule->is_past ? 'secondary' : 'primary' }}">
-                {{ $schedule->title }}
-              </a>
-              <br/>
-              <small>
-                @if ($schedule->is_virtual OR empty($schedule->full_location))
-                  <i class="ri-earth-line"></i>
-                @else
-                  {!! $schedule->full_location !!}
-                @endif
-              </small>
-              <br/>
-              <small class="small-screen">
+            <th scope="col">@lang('event.schedule_date')</th>
+            <th scope="col">@lang('event.schedule_title')</th>
+            <th scope="col" class="large-screen">@lang('event.schedule_categories')</th>
+          </tr>
+          </thead>
+          <tbody>
+          @foreach($schedules as $schedule)
+            <tr>
+              <td>
+                <div class="pico-color-{{ get_color_theme() }}-600">{{ $schedule->started_at->timezone($timezone)->translatedFormat('d') }}</div>
+                {{ $schedule->started_at->timezone($timezone)->format('M') }}<sup>{{ $schedule->started_at->timezone($timezone)->format('y') }}</sup>
+              </td>
+              <td>
+                <a href="{{ route('schedule.view', ['slug' => $schedule->slug]) }}" class="{{ $schedule->is_past ? 'secondary' : 'primary' }}">
+                  {{ $schedule->title }}
+                </a>
+                <br/>
+                <small>
+                  @if ($schedule->is_virtual OR empty($schedule->full_location))
+                    <i class="ri-earth-line"></i>
+                  @else
+                    {!! $schedule->full_location !!}
+                  @endif
+                </small>
+                <br/>
+                <small class="small-screen">
+                  @foreach($schedule->categories as $category)
+                    <mark>{{ $category }}</mark>
+                  @endforeach
+                </small>
+              </td>
+              <td class="large-screen">
                 @foreach($schedule->categories as $category)
                   <mark>{{ $category }}</mark>
                 @endforeach
-              </small>
-            </td>
-            <td class="large-screen">
-              @foreach($schedule->categories as $category)
-                <mark>{{ $category }}</mark>
-              @endforeach
-            </td>
-          </tr>
-        @endforeach
-        </tbody>
-      </table>
-    </div>
+              </td>
+            </tr>
+          @endforeach
+          </tbody>
+        </table>
+      </div>
+    </article>
   @endif
 </div>
