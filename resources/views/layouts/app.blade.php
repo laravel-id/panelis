13f020f1c9c71a394bf6bd25d77d02f1f144088a
Yugo <dedy.yugo.purwanto@gmail.com>
2024-08-20 16:32:46 +0700

    Remove Google Tag Manager & add analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,23 +13,8 @@
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @stack('js')
-
-	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-MHN9VTF6');</script>
-	<!-- End Google Tag Manager -->
-
-  <title>{{ $pageTitle ?? $title ?? config('app.name') }}</title>
 </head>
 <body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHN9VTF6"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-
 <main class="container">
   <nav>
     <ul>
@@ -48,5 +33,8 @@
 
   @yield('content')
 </main>
+<!-- 100% privacy-first analytics -->
+<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+<noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
 </body>
 </html>
