f737efd9423be9358dc1171a9afe4b70ce0db582
Yugo <dedy.yugo.purwanto@gmail.com>
2024-08-20 16:23:08 +0700

    Add Google Tag Manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,22 @@
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @stack('js')
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MHN9VTF6');</script>
+	<!-- End Google Tag Manager -->
+
   <title>{{ $pageTitle ?? $title ?? config('app.name') }}</title>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHN9VTF6"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <main class="container">
   <nav>
     <ul>
