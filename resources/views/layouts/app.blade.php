919893110ea3ea4bbf63d7d812de50f64ce7c9e9
Yugo <dedy.yugo.purwanto@gmail.com>
2024-08-20 17:16:27 +0700

    Fix missing title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,9 @@
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.{{ get_color_theme() }}.min.css"/>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css" />
+	<title>{{ $pageTitle ?? $title ?? config('app.name') }}</title>
 
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
   @stack('js')
 </head>
 <body>
