cc466e0786aa5d0cb47512179f5974d0640acb91
Yugo <dedy.yugo.purwanto@gmail.com>
2024-10-16 19:41:02 +0700

    Add custom middleware to authorize log viewer

---
+++ app/Http/Middleware/ViewLogs.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ViewLogs
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        abort_if(! $request->user()->is_root || ! $request->user()->can('ViewLogs'), Response::HTTP_FORBIDDEN);
+
+        return $next($request);
+    }
+}
--- config/log-viewer.php
+++ config/log-viewer.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\ViewLogs;
+
 return [
 
     /*
@@ -73,7 +75,7 @@ return [
 
     'middleware' => [
         'web',
-        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        ViewLogs::class,
     ],
 
     /*
