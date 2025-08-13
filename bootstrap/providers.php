126ca9151c07de8f3ee416fb79b81c46077d27a2
Yugo <dedy.yugo.purwanto@gmail.com>
2025-08-13 11:13:53 +0700

    Order admin panel provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,11 +4,11 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\EventServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FilesystemServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
     App\Providers\SettingServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
     Spatie\TranslationLoader\TranslationServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
 ];
